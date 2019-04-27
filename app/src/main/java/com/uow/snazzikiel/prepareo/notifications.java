0c744ebb07507027a1c299b91f93c5289e142b24
Snazzikiel <azzifd@gmail.com>
2019-04-27 15:20:32 +1000

    Notification File altered - Commit update 27/4/19

---
+++ app/src/main/java/com/uow/snazzikiel/prepareo/notifications.java
@@ -0,0 +1,13 @@
+package com.uow.snazzikiel.prepareo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class notifications extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notifications);
+    }
+}
