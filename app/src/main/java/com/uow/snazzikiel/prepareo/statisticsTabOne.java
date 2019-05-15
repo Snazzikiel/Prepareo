732c2b9a2d3a07f7812e3b263e265ea2a28d8ef5
Snazzikiel <azzifd@gmail.com>
2019-05-16 01:44:00 +1000

    Adding statistics tab information for Tab 1 and 2 - added graph but no reflection of actual data.

--- app/src/main/java/com/uow/snazzikiel/prepareo/statisticsTabOne.java
+++ app/src/main/java/com/uow/snazzikiel/prepareo/statisticsTabOne.java
@@ -7,8 +7,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import lecho.lib.hellocharts.model.PieChartData;
+import lecho.lib.hellocharts.model.SliceValue;
+import lecho.lib.hellocharts.view.PieChartView;
 
 
 public class statisticsTabOne extends Fragment {
@@ -16,11 +21,15 @@ public class statisticsTabOne extends Fragment {
 
     private Button btnTEST;
 
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.statistics_tab1,container,false);
-        btnTEST = (Button) view.findViewById(R.id.btnTEST);
+
+        createPieChartRed(view);
+        createPieChartWhite(view);
+        /*btnTEST = (Button) view.findViewById(R.id.btnTEST);
 
         btnTEST.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,8 +37,36 @@ public class statisticsTabOne extends Fragment {
                 Toast.makeText(getActivity(), "TESTING BUTTON CLICK 1",Toast.LENGTH_SHORT).show();
             }
         });
-
+        */
         return view;
+    }
+
+    public void createPieChartRed(View v){
+
+        PieChartView pieChartView = v.findViewById(R.id.chart1);
+        List<SliceValue> pieData = new ArrayList<>();
+        pieData.add(new SliceValue(75, R.color.colorWhite));
+        pieData.add(new SliceValue(25, R.color.colorLightBlue));
+
+        PieChartData pieChartData = new PieChartData(pieData);
+        pieChartData.setHasCenterCircle(true).setCenterText1("70%").setCenterText1FontSize(18).setCenterText1Color(R.color.colorBlack);
+
+        pieChartView.setPieChartData(pieChartData);
+    }
+
+    public void createPieChartWhite(View v){
+
+        int chartValue = 70;
+
+        PieChartView pieChartView = v.findViewById(R.id.chart2);
+        List<SliceValue> pieData = new ArrayList<>();
+        pieData.add(new SliceValue(chartValue, R.color.colorRed));
+        pieData.add(new SliceValue((100 - chartValue), R.color.colorLightBlue));
+
+        PieChartData pieChartData = new PieChartData(pieData);
+        pieChartData.setHasCenterCircle(true).setCenterText1(Integer.toString(chartValue)).setCenterText1FontSize(18).setCenterText1Color(R.color.colorBlack);
+
+        pieChartView.setPieChartData(pieChartData);
     }
 }
 
--- app/src/main/java/com/uow/snazzikiel/prepareo/statisticsTabTwo.java
+++ app/src/main/java/com/uow/snazzikiel/prepareo/statisticsTabTwo.java
@@ -9,27 +9,232 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import lecho.lib.hellocharts.listener.ComboLineColumnChartOnValueSelectListener;
+import lecho.lib.hellocharts.model.Axis;
+import lecho.lib.hellocharts.model.Column;
+import lecho.lib.hellocharts.model.ColumnChartData;
+import lecho.lib.hellocharts.model.ComboLineColumnChartData;
+import lecho.lib.hellocharts.model.Line;
+import lecho.lib.hellocharts.model.LineChartData;
+import lecho.lib.hellocharts.model.PointValue;
+import lecho.lib.hellocharts.model.SubcolumnValue;
+import lecho.lib.hellocharts.util.ChartUtils;
+import lecho.lib.hellocharts.view.ComboLineColumnChartView;
 
 
 public class statisticsTabTwo extends Fragment {
     private static final String TAG = "Tab2Fragment";
 
-    private Button btnTEST;
+    private ComboLineColumnChartView clChartView;
+    private ComboLineColumnChartData data;
+    private int numberOfLines = 1;
+    private int maxNumberOfLines = 4;
+    private int numberOfPoints = 12;
+
+    float[][] randomNumbersTab = new float[maxNumberOfLines][numberOfPoints];
+
+    private boolean hasAxes = true;
+    private boolean hasAxesNames = true;
+    private boolean hasPoints = true;
+    private boolean hasLines = true;
+    private boolean isCubic = false;
+    private boolean hasLabels = false;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.statistics_tab2,container,false);
-        btnTEST = (Button) view.findViewById(R.id.btnTEST2);
 
-        btnTEST.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getActivity(), "TESTING BUTTON CLICK 2",Toast.LENGTH_SHORT).show();
-            }
-        });
+        clChartView = (ComboLineColumnChartView) view.findViewById(R.id.statistics_tab2_mainChart);
+        clChartView.setOnValueTouchListener(new ValueTouchListener());
+
+        generateValues();
+        generateData();
 
         return view;
     }
+
+
+    private void generateValues() {
+        for (int i = 0; i < maxNumberOfLines; ++i) {
+            for (int j = 0; j < numberOfPoints; ++j) {
+                randomNumbersTab[i][j] = (float) Math.random() * 50f + 5;
+            }
+        }
+    }
+
+    private void reset() {
+        numberOfLines = 1;
+
+        hasAxes = true;
+        hasAxesNames = true;
+        hasLines = true;
+        hasPoints = true;
+        hasLabels = false;
+        isCubic = false;
+
+    }
+
+    private void generateData() {
+        // Chart looks the best when line data and column data have similar maximum viewports.
+        data = new ComboLineColumnChartData(generateColumnData(), generateLineData());
+
+        if (hasAxes) {
+            Axis axisX = new Axis();
+            Axis axisY = new Axis().setHasLines(true);
+            if (hasAxesNames) {
+                axisX.setName("Axis X");
+                axisY.setName("Axis Y");
+            }
+            data.setAxisXBottom(axisX);
+            data.setAxisYLeft(axisY);
+        } else {
+            data.setAxisXBottom(null);
+            data.setAxisYLeft(null);
+        }
+
+        clChartView.setComboLineColumnChartData(data);
+    }
+
+    private LineChartData generateLineData() {
+
+        List<Line> lines = new ArrayList<Line>();
+        for (int i = 0; i < numberOfLines; ++i) {
+
+            List<PointValue> values = new ArrayList<PointValue>();
+            for (int j = 0; j < numberOfPoints; ++j) {
+                values.add(new PointValue(j, randomNumbersTab[i][j]));
+            }
+
+            Line line = new Line(values);
+            line.setColor(ChartUtils.COLORS[i]);
+            line.setCubic(isCubic);
+            line.setHasLabels(hasLabels);
+            line.setHasLines(hasLines);
+            line.setHasPoints(hasPoints);
+            lines.add(line);
+        }
+
+        LineChartData lineChartData = new LineChartData(lines);
+
+        return lineChartData;
+
+    }
+
+    private ColumnChartData generateColumnData() {
+        int numSubcolumns = 1;
+        int numColumns = 12;
+        // Column can have many subcolumns, here by default I use 1 subcolumn in each of 8 columns.
+        List<Column> columns = new ArrayList<Column>();
+        List<SubcolumnValue> values;
+        for (int i = 0; i < numColumns; ++i) {
+
+            values = new ArrayList<SubcolumnValue>();
+            for (int j = 0; j < numSubcolumns; ++j) {
+                values.add(new SubcolumnValue((float) Math.random() * 50 + 5, ChartUtils.COLOR_GREEN));
+            }
+
+            columns.add(new Column(values));
+        }
+
+        ColumnChartData columnChartData = new ColumnChartData(columns);
+        return columnChartData;
+    }
+
+    private void addLineToData() {
+        if (data.getLineChartData().getLines().size() >= maxNumberOfLines) {
+            Toast.makeText(getActivity(), "Samples app uses max 4 lines!", Toast.LENGTH_SHORT).show();
+            return;
+        } else {
+            ++numberOfLines;
+        }
+
+        generateData();
+    }
+
+    private void toggleLines() {
+        hasLines = !hasLines;
+
+        generateData();
+    }
+
+    private void togglePoints() {
+        hasPoints = !hasPoints;
+
+        generateData();
+    }
+
+    private void toggleCubic() {
+        isCubic = !isCubic;
+
+        generateData();
+    }
+
+    private void toggleLabels() {
+        hasLabels = !hasLabels;
+
+        generateData();
+    }
+
+    private void toggleAxes() {
+        hasAxes = !hasAxes;
+
+        generateData();
+    }
+
+    private void toggleAxesNames() {
+        hasAxesNames = !hasAxesNames;
+
+        generateData();
+    }
+
+    private void prepareDataAnimation() {
+
+        // Line animations
+        for (Line line : data.getLineChartData().getLines()) {
+            for (PointValue value : line.getValues()) {
+                // Here I modify target only for Y values but it is OK to modify X targets as well.
+                value.setTarget(value.getX(), (float) Math.random() * 50 + 5);
+            }
+        }
+
+        // Columns animations
+        for (Column column : data.getColumnChartData().getColumns()) {
+            for (SubcolumnValue value : column.getValues()) {
+                value.setTarget((float) Math.random() * 50 + 5);
+            }
+        }
+    }
+
+    private class ValueTouchListener implements ComboLineColumnChartOnValueSelectListener {
+
+        @Override
+        public void onValueDeselected() {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void onColumnValueSelected(int columnIndex, int subcolumnIndex, SubcolumnValue value) {
+            Toast.makeText(getActivity(), "Selected column: " + value, Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onPointValueSelected(int lineIndex, int pointIndex, PointValue value) {
+            Toast.makeText(getActivity(), "Selected line point: " + value, Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    /* MENU
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.combo_line_column_chart, menu);
+    }*/
+
+
 }
 
